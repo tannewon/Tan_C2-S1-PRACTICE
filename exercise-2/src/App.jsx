68433154ed09d780b126462e892e549319c152f0
Tan <tan.a25@student.passerellesnumeriques.org>
2024-05-25 09:57:55 +0700

    exercise-2

--- exercise-2/src/App.jsx
+++ exercise-2/src/App.jsx
@@ -13,11 +13,11 @@ export const myData = {
 };
 
 // TODO Edit the User component code to be able to display DIFFERENT users !
-export function User() {
+export function User({ firstName, lastName, title }) {
   return (
     <div id="user" data-testid="user">
-      <h2>TODO: FIRST NAME LAST NAME</h2>
-      <p>TODO: TITLE</p>
+      <h2>{firstName} {lastName}</h2>
+      <p>{title}</p>
     </div>
   );
 }
@@ -27,8 +27,8 @@ function App() {
     <div id="app">
       <h1>PNV React Team !!</h1>
       <p>Here are some PNV React masters, do you know them?</p>
-      <User /> {/* This line needs some changes*/}
-      <User /> {/* This line needs some changes*/}
+      <User firstName={vinhData.firstName} lastName={vinhData.lastName} title={vinhData.title} />
+      <User firstName={myData.firstName} lastName={myData.lastName} title={myData.title} />
     </div>
   );
 }
